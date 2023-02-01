fb17015381bdd4d60253dbfce648a242ea8674fc
Luke Walsh <kyon.rf@gmail.com>
2023-02-01 08:56:21 +0000

    If we have host in the string, then don't append again.

--- src/resources/views/auth/token.blade.php
+++ src/resources/views/auth/token.blade.php
@@ -39,7 +39,7 @@
         <script>
             const host = new URLSearchParams(location.search).get("host")
             utils.getSessionToken(app).then((token) => {
-                window.location.href = `{!! $target !!}{!! Str::contains($target, '?') ? '&' : '?' !!}token=${token}`;
+                window.location.href = `{!! $target !!}{!! Str::contains($target, '?') ? '&' : '?' !!}token=${token}&host=${host}`;
             });
         </script>
     @endif
