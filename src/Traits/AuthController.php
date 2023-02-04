cd1f431222f9b908ba7631cc1190501e77f03d9c
Rishabh Tayal <rtayal11@gmail.com>
2023-02-04 08:55:43 -0600

    Update AuthController.php

--- src/Traits/AuthController.php
+++ src/Traits/AuthController.php
@@ -63,7 +63,7 @@ trait AuthController
                     'apiKey' => Util::getShopifyConfig('api_key', $shopOrigin),
                     'appBridgeVersion' => Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '',
                     'authUrl' => $result['url'],
-                    'host' => $request->get('host'),
+                    'host' => $request->get('host') ?? base64_decode($shopDomain),
                     'shopDomain' => $shopDomain,
                 ]
             );
