b124e4fec0f54b885b941b6fa34bd3d2c72c59cc
Luke Walsh <kyon.rf@gmail.com>
2023-08-24 11:07:19 +0100

    Stub out the error for the middlware and set up the test for coverage

--- tests/Http/Middleware/VerifyScopesTest.php
+++ tests/Http/Middleware/VerifyScopesTest.php
@@ -42,4 +42,48 @@ class VerifyScopesTest extends TestCase
         //this line needs to assert if proper redirect was made
         $this->assertEquals(302, $result->getStatusCode());
     }
+
+    public function testMatchingScopes(): void
+    {
+        // Setup API stub
+        $this->setApiStub();
+        ApiStub::stubResponses(['access_scopes']);
+
+        $this->app['config']->set('shopify-app.api_scopes', 'read_products,write_products');
+
+        $shop = factory($this->model)->create();
+        $this->auth->login($shop);
+
+        $request = Request::create('/', 'GET', ['shop' => $shop->getDomain()->toNative()]);
+
+        // Run the middleware
+        $middleware = new VerifyScopesMiddleware();
+        $result = $middleware->handle($request, function () {
+        });
+
+        //this line needs to assert if proper redirect was made
+        $this->assertEquals($result, null);
+    }
+
+    public function testScopeApiFailure(): void
+    {
+        // Setup API stub
+        $this->setApiStub();
+        ApiStub::stubResponses(['access_scopes_error']);
+
+        $this->app['config']->set('shopify-app.api_scopes', 'read_products,write_products');
+
+        $shop = factory($this->model)->create();
+        $this->auth->login($shop);
+
+        $request = Request::create('/', 'GET', ['shop' => $shop->getDomain()->toNative()]);
+
+        // Run the middleware
+        $middleware = new VerifyScopesMiddleware();
+        $result = $middleware->handle($request, function () {
+        });
+
+        //this line needs to assert if proper redirect was made
+        $this->assertEquals($result, null);
+    }
 }
