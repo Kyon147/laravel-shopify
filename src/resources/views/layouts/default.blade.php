43f711e3b7e00cf1d6497469c4c085294dd76c8d
Gulshan <17927306+Guley@users.noreply.github.com>
2024-09-24 14:19:19 +0530

    Update default.blade.php
    Update app bridge and util as per the new update from the shopify

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -19,7 +19,8 @@
 
         @if(\Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_enabled') && \Osiset\ShopifyApp\Util::useNativeAppBridge())
             <script src="{{config('shopify-app.appbridge_cdn_url') ?? 'https://unpkg.com'}}/@shopify/app-bridge{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
-            <script
+             <script src="{{config('shopify-app.appbridge_cdn_url') ?? 'https://unpkg.com'}}/@shopify/app-bridge-utils{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_util_version') ? '@'.config('shopify-app.appbridge_util_version') : '' }}"></script>
+        <script
                 @if(\Osiset\ShopifyApp\Util::getShopifyConfig('turbo_enabled'))
                     data-turbolinks-eval="false"
                 @endif
@@ -30,7 +31,7 @@
                 var createApp = AppBridge.default;
                 var app = createApp({
                     apiKey: "{{ \Osiset\ShopifyApp\Util::getShopifyConfig('api_key', $shopDomain ?? Auth::user()->name ) }}",
-                    host: "{{ \Request::get('host') }}",
+                    host: "{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_host') ? config('shopify-app.appbridge_host') : '' }}",
                     forceRedirect: true,
                 });
             </script>
