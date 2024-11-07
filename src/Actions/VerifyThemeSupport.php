f5afea8dfb94da203950cde194a23becdbcb8f92
enmaboya <vita-dubov@yandex.ru>
2024-11-07 18:24:50 +0400

    remove theme helper

--- src/Actions/VerifyThemeSupport.php
+++ src/Actions/VerifyThemeSupport.php
@@ -1,79 +1,137 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Osiset\ShopifyApp\Actions;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Osiset\ShopifyApp\Contracts\Queries\Shop as IShopQuery;
+use Osiset\ShopifyApp\Contracts\ShopModel;
 use Osiset\ShopifyApp\Objects\Enums\ThemeSupportLevel;
 use Osiset\ShopifyApp\Objects\Values\ShopId;
-use Osiset\ShopifyApp\Services\ThemeHelper;
+use Osiset\ShopifyApp\Util;
 
-/**
- * Activates a plan for a shop.
- */
-class VerifyThemeSupport
+final class VerifyThemeSupport
 {
-    /**
-     * Querier for shops.
-     *
-     * @var IShopQuery
-     */
-    protected $shopQuery;
+    private const ASSET_FILE_NAMES = ['templates/product.json', 'templates/collection.json', 'templates/index.json'];
 
-    /**
-     * Theme helper.
-     *
-     * @var ThemeHelper
-     */
-    protected $themeHelper;
+    private const MAIN_ROLE = 'main';
 
-    /**
-     * Setup.
-     *
-     * @param IShopQuery  $shopQuery   The querier for shops.
-     * @param ThemeHelper $themeHelper Theme helper.
-     *
-     * @return void
-     */
+    private string $cacheInterval;
+
+    private int $cacheDuration;
+
     public function __construct(
-        IShopQuery $shopQuery,
-        ThemeHelper $themeHelper
+        private IShopQuery $shopQuery,
+        private FetchMainTheme $fetchMainTheme,
+        private FetchThemeAssets $fetchThemeAssets,
     ) {
-        $this->shopQuery = $shopQuery;
-        $this->themeHelper = $themeHelper;
+        $this->cacheInterval = (string) Str::of(Util::getShopifyConfig('theme_support.cache_interval'))
+            ->plural()
+            ->ucfirst()
+            ->start('add');
+
+        $this->cacheDuration = (int) Util::getShopifyConfig('theme_support.cache_duration');
     }
 
-    /**
-     * Execution.
-     *
-     * @param ShopId $shopId The shop ID.
-     *
-     * @return int
-     */
     public function __invoke(ShopId $shopId): int
     {
-        $this->themeHelper->extractStoreMainTheme($shopId);
+        $shop = $this->shopQuery->getById($shopId);
 
-        if ($this->themeHelper->themeIsReady()) {
-            $templateJSONFiles = $this->themeHelper->templateJSONFiles();
-            $templateMainSections = $this->themeHelper->mainSections($templateJSONFiles);
-            $sectionsWithAppBlock = $this->themeHelper->sectionsWithAppBlock($templateMainSections);
+        /** @var array{id: string, name: string} */
+        $mainTheme = Cache::remember(
+            "mainTheme.{$shop->getId()->toNative()}",
+            now()->{$this->cacheInterval}($this->cacheDuration),
+            fn () => $this->fetchMainTheme->handle($shop)
+        );
 
-            $hasTemplates = count($templateJSONFiles) > 0;
-            $allTemplatesHasRightType = count($templateJSONFiles) === count($sectionsWithAppBlock);
-            $templatesСountWithRightType = count($sectionsWithAppBlock);
+        if (isset($mainTheme['id'])) {
+            /** @var array<int, array{filename: string, content: string}> */
+            $assets = Cache::remember(
+                "assets.{$mainTheme['id']}.{$shop->getId()->toNative()}",
+                now()->{$this->cacheInterval}($this->cacheDuration),
+                fn () => $this->fetchThemeAssets->handle(
+                    shop: $shop,
+                    mainThemeId: $mainTheme['id'],
+                    filenames: self::ASSET_FILE_NAMES
+                )
+            );
+            $templateMainSections = $this->mainSections(
+                shop: $shop,
+                mainTheme: $mainTheme,
+                assets: $assets
+            );
+            $sectionsWithAppBlock = $this->sectionsWithAppBlock($templateMainSections);
 
-            switch (true) {
-                case $hasTemplates && $allTemplatesHasRightType:
-                    return ThemeSupportLevel::FULL;
+            $hasTemplates = count($assets) > 0;
+            $allTemplatesHasRightType = count($assets) === count($sectionsWithAppBlock);
+            $hasTemplatesCountWithRightType = count($sectionsWithAppBlock) > 0;
 
-                case $templatesСountWithRightType:
-                    return ThemeSupportLevel::PARTIAL;
-
-                default:
-                    return ThemeSupportLevel::UNSUPPORTED;
-            }
+            return match (true) {
+                $hasTemplates && $allTemplatesHasRightType => ThemeSupportLevel::FULL,
+                $hasTemplatesCountWithRightType => ThemeSupportLevel::PARTIAL,
+                default => ThemeSupportLevel::UNSUPPORTED
+            };
         }
 
         return ThemeSupportLevel::UNSUPPORTED;
+    }
+
+    /**
+     * @template T
+     * @template Z
+     *
+     * @param  Z  $mainTheme
+     * @param  T  $assets
+     * @return T
+     */
+    private function mainSections(ShopModel $shop, array $mainTheme, array $assets): array
+    {
+        $filenamesForMainSections = array_filter(
+            array_map(function ($asset) {
+                $assetContent = json_decode($asset['content'], true);
+
+                $mainAsset = array_filter($assetContent['sections'], function ($value, $key) {
+                    return $key == self::MAIN_ROLE || str_starts_with($value['type'], self::MAIN_ROLE);
+                }, ARRAY_FILTER_USE_BOTH);
+
+                if ($mainAsset) {
+                    return 'sections/' . end($mainAsset)['type'] . '.liquid';
+                }
+            }, $assets)
+        );
+
+        return Cache::remember(
+            "mainSections.{$mainTheme['id']}." . sha1(implode('|', $filenamesForMainSections)),
+            now()->{$this->cacheInterval}($this->cacheDuration),
+            fn () => $this->fetchThemeAssets->handle(
+                shop: $shop,
+                mainThemeId: $mainTheme['id'],
+                filenames: [...$filenamesForMainSections]
+            )
+        );
+    }
+
+    /**
+     * @template T
+     *
+     * @param  T  $templateMainSections
+     * @return T
+     */
+    private function sectionsWithAppBlock(array $templateMainSections): array
+    {
+        return array_filter(array_map(function ($file) {
+            $acceptsAppBlock = false;
+
+            preg_match('/\{\%-?\s+schema\s+-?\%\}([\s\S]*?)\{\%-?\s+endschema\s+-?\%\}/m', $file['content'], $matches);
+            $schema = json_decode($matches[1] ?? '{}', true);
+
+            if ($schema && isset($schema['blocks'])) {
+                $acceptsAppBlock = in_array('@app', array_column($schema['blocks'], 'type'));
+            }
+
+            return $acceptsAppBlock ? $file : null;
+        }, $templateMainSections));
     }
 }
